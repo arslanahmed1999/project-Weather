6ee04eead9b2cdb6c0278b359ce731a815accfa8
Arslan Ahmed <149689337+arslanahmed1999@users.noreply.github.com>
2023-11-10 19:07:08 +0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,86 @@
+
+ 
+
+#sum of numbers
+#first_number =  input("Enter First Number: " )
+#second_number =  input("Enter Second Number: " )
+#sum = int(first_number) + int(second_number)
+
+#print(f'Sum of the number {sum}')
+
+
+# tuple example
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[:3])   
+
+#thisset = {"apple", "banana", "cherry"}
+
+#thisset.remove("apple")
+
+#print(thisset)
+
+
+
+#thisdict = {
+ # "brand": "Ford",
+  #"electric": False,
+ # "year": 1964,
+  #"colors": ["red", "white", "blue"]
+#}
+
+#print(thisdict)
+#thisdict =	{
+ # "brand": "Ford",
+ # "model": "Mustang",
+ # "year": 1964
+#}
+#x = thisdict["model"]
+#print(x)
+                                                                          #Quiz Game
+
+
+#print("Welcome to my computer Quiz Game ")
+
+#playing = input("Do You Want To Play? ")
+#if playing.lower() != "yes":
+   #     quit()
+
+#print("okay lets play") 
+#score = 0
+
+#answer = input("what does RAM stands for? " )
+#if answer.lower() == "random access memory":
+  #   print("correct :)")
+#else:
+  #     print("Wrong :(")
+
+#       answer = input("what does CPU stands for? " )
+#if answer.lower() == "Central processing unit":
+ #    print("correct :)")
+#else:
+ #      print("Wrong :(")
+
+  #     answer = input("what does ppc stands for? " )
+#if answer.lower() == "pay-per-click":
+ #    print("correct :)")
+#else:
+ #      print("Wrong :(")
+
+ #      answer = input("what does API stands for? " )
+#if answer.lower() == "application programming interface":
+ #    print("correct :)")
+#else:
+ #      print("Wrong :(")
+
+  #     answer = input("what does AWP stands for? " )
+#if answer.lower() == "Association of Writers and Writing Programs":
+ #    print("correct :)")
+#else:
+ #      print("Wrong :(")
+
+                                                                             
+                                                                                    #Random Number Guessing Game
+
+import random
+random_number = random.randint(-5, 10)
+print(random_number)
